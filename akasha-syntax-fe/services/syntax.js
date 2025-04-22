546ccf12a942d1b797c8e5f07e06049429be874e
Talat Mahmud <talatcbf@gmail.com>
2025-04-22 09:03:01 +0600

    fix: endpoint

--- akasha-syntax-fe/services/syntax.js
+++ akasha-syntax-fe/services/syntax.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { noAuthHeader } from "@/services/headers";
 
-const BASE_URL = "https://li6dit4n0lz7.share.zrok.io";
+const BASE_URL = "https://api.akashasolutions.com";
 
 export const ask = async (data) => {
   return await axios.post(`${BASE_URL}/ask`, data, {
